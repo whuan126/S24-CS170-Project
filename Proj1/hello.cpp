f6d974b983fd95e443650fd8f9f6c4999fcf8682
VladimirKataev <vova.kataev@gmail.com>
2024-04-12 19:04:58 -0700

    Started working on file

--- Proj1/hello.cpp
+++ Proj1/hello.cpp
@@ -1,1 +1,9 @@
-// Hi
+// Compile using g++ hello.cpp, run using ./a.out
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+int main(int argc, char** argv){
+    cout << "Hi " << argv[0] << endl;
+    return 0;
+}
